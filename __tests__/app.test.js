b7ef43f8e92bcb9c15682d6e420b84088f83106d
robharmer3 <rob.harmer3@gmail.com>
2025-03-10 10:04:25 +0000

    Ticket 1 - CORE: GET /api - Completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,10 @@
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+const request = require("supertest")
+const app = require("../app")
 
 /* Set up your beforeEach & afterAll functions here */
 
-describe.skip("GET /api", () => {
+describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
