90e08f51b87b34b10b052ef18e7aee1bba687c2f
robharmer3 <rob.harmer3@gmail.com>
2025-03-13 16:59:04 +0000

    ticket 21 completed

--- controllers/controller.topics.js
+++ controllers/controller.topics.js
@@ -1,8 +1,20 @@
-const { fetchAllTopics } = require("../models/model.topics")
+const { fetchAllTopics, createTopic } = require("../models/model.topics")
 
 exports.getAllTopics = (request, response) => {
     fetchAllTopics()
     .then((topics) => {
         response.status(200).send({ topics })
     })
+}
+
+exports.postTopic = (request, response, next) => {
+    const {slug, description} = request.body
+    createTopic(slug, description)
+    .then((topic) => {
+        response.status(201).send({topic})
+    })
+    .catch((error) => {
+        next(error)
+    })
+    
 }
--- models/model.topics.js
+++ models/model.topics.js
@@ -7,3 +7,20 @@ exports.fetchAllTopics = () => {
         return rows
     })
 }
+
+exports.createTopic = (slug, description) => {
+    if (slug && description){
+        return db.query(`
+            INSERT INTO topics
+            (slug, description)
+            VALUES
+            ($1, $2)
+            RETURNING *`,
+            [slug, description])
+        .then(({rows}) => {
+            return rows[0]
+        })
+    } else {
+        return Promise.reject({status: 400, msg: "Bad Request, invalid input"})
+    }
+}
